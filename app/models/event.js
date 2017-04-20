f2ae95044e368a1588337c45e4c4b2cbcbb56972
Salvador Alfocea <salvadoralfoceadominguez@gmail.com>
2017-04-20 17:38:21 +0200

    Update event.js

--- app/models/event.js
+++ app/models/event.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 /***** CREATE A SCHEMA **********************************/
 
 const eventSchema = new Schema({
@@ -16,53 +15,23 @@ const eventSchema = new Schema({
 	created: {
 		type: Date,
 		default: Date.now()
-		// select: false ==> significa que por defecto no devuelve este campo. 
-		// Recomendado para datos sensibles, como por ejemplo una contraseña.
 	} 
 });
 
-
 /***** MIDDLEWARE ***************************************/
 
-// Make sure that the slug is created from the name
-eventSchema.pre('save', function(next) { // esta función se dispara antes de ejecutar el evento 'save'.
+eventSchema.pre('save', function(next) {
 	this.slug = slugify(this.name);
 	next();
 });
-
-
-/***** METHODS ******************************************/
-
-eventSchema.methods.speak = function () {
-  	var greeting = this.name
-    	? "Meow name is " + this.name
-    	: "I don't have a name";
-  	
-  	console.log(greeting);
-
-  	/*
-		How to use:
-
-		var event = new Event({ name: 'fluffy' });
-		event.speak(); // "Meow name is fluffy"
-
-		event.save(function (err, r_event) {
-		  	if (err) return console.error(err);
-		  	r_event.speak();
-		});
-  	*/
-}
-
 
 /***** CREATE THE MODEL *********************************/
 
 const eventModel = mongoose.model('Event', eventSchema);
 
-
 /***** EXPORT THE MODEL *********************************/
 
 module.exports = eventModel;
-
 
 /***** FUNCTIONS ****************************************/
 
